3bdfacfc63d65eca6decf1b4f87e5ed6ecfddc5a
Setemi Loye <setemi@Setemis-MacBook-Pro.local>
2025-08-20 23:18:03 +0100

    First commit into the dev branch

---
+++ app/notifications/page.tsx
@@ -0,0 +1,10 @@
+import ProfileNav from "@/components/profiie-nav";
+
+export default function Page() {
+  return (
+      <div>
+          <ProfileNav />
+      <h1>Notifications Page</h1>
+    </div>
+  );
+}
